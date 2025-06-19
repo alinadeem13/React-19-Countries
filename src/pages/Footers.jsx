be6ff97a399222cd8efa4247c6db0c1f7a95c0fd
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-06-19 18:21:46 +0500

    Footer Component Implemented

--- src/pages/Footers.jsx
+++ src/pages/Footers.jsx
@@ -1,7 +1,19 @@
+import footerContact from "../api/footerApi";
 export const Footers = () => {
   return (
-    <div>
-      <h1>Welcome to the Footers Page</h1>
-    </div>
+    <footer className="footer-section">
+      <div className="container grid grid-four-cols">
+        {footerContact.map((curData, index) => {
+          const { icon, title, description } = curData;
+          return (
+            <div className="footer-contact" key={index}>
+              <div className="footer-icon">{icon}</div>
+              <h3 className="footer-title">{title}</h3>
+              <p className="footer-description">{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </footer>
   );
 };
